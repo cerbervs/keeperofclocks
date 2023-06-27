f73cecd0439543f30842df1ab6e3db530133d0cc
Mark Bailey <mark.bailey@moyerautogroup.com>
2023-06-27 12:14:36 -0400

    Intro functionality for main.go

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "github.com/cerbervs/keeperofclocks/logging"
+
 func main() {
-	// TODO: Add code here
+	timeLogger := logging.TimeLogger{}
+	timeLogger.Init("test")
+	defer timeLogger.File.Close()
+
+	print(
+		timeLogger.Info().Time.String() + "\n" + timeLogger.Info().Username + "\n" + timeLogger.Info().Filename + "\n",
+	)
 }
